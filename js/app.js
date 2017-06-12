1638055cfd9d2f1588cdde04e26ad53ecd958be9
Skillz <admin@ezlgg.com>
2017-06-11 23:11:49 -0400

    completed start screen requirements and began work on second set

--- js/app.js
+++ js/app.js
@@ -1,1 +1,27 @@
-app.js
+(function() {
+const board = document.getElementById('board');
+const player1 = document.getElementById('player1');
+const player2 = document.getElementById('player2');
+const boxes = document.getElementsByClassName('boxes');
+const finish = document.getElementById('finish');
+const startHeader = $('#start header h1');
+const startButton = document.querySelector('.button');
+const start = document.getElementById('start');
+
+startHeader.after('<br><br><input type="text" placeholder="Player Name" id="player"><br><br>');
+finish.style.display = 'none';
+board.style.display = 'none';
+
+const player = document.getElementById('player');
+
+startButton.addEventListener('click', () => {
+     const playerName = player.value;
+     start.style.display = 'none';
+     board.style.display = '';
+     player1
+
+})
+
+})()
+
+// console.log(startButton);
