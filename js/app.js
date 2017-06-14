ffe12898960b141c73d2ddefff3493308c08fef7
Skillz4Killz <admin@ezlgg.com>
2017-06-14 12:27:49 -0400

    added in more functions

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 (function ticTacToeGame(){
 	const board = document.getElementById('board');
 	const start = document.getElementById('start');
@@ -114,7 +114,7 @@
 		})
 	}
 
-	function clicking (x, y) {
+	function clicking (x) {
 		x.addEventListener('click', () => {
 			if (player1.className === 'players active') {
 				x.className += ' box-filled-1';
@@ -154,41 +154,16 @@
 	endButton.addEventListener('click', () => {
 		finish.style.display = 'none';
 		board.style.display = '';
-		
+		box0.className = 'box';
+		box1.className = 'box';
+		box2.className = 'box';
+		box3.className = 'box';
+		box4.className = 'box';
+		box5.className = 'box';
+		box6.className = 'box';
+		box7.className = 'box';
+		box8.className = 'box';
 	})
-
-
-
-
 
 })()
 // console.log('testing');
-=======
-(function() {
-const board = document.getElementById('board');
-const player1 = document.getElementById('player1');
-const player2 = document.getElementById('player2');
-const boxes = document.getElementsByClassName('boxes');
-const finish = document.getElementById('finish');
-const startHeader = $('#start header h1');
-const startButton = document.querySelector('.button');
-const start = document.getElementById('start');
-
-startHeader.after('<br><br><input type="text" placeholder="Player Name" id="player"><br><br>');
-finish.style.display = 'none';
-board.style.display = 'none';
-
-const player = document.getElementById('player');
-
-startButton.addEventListener('click', () => {
-     const playerName = player.value;
-     start.style.display = 'none';
-     board.style.display = '';
-     player1
-
-})
-
-})()
-
-// console.log(startButton);
->>>>>>> 1638055cfd9d2f1588cdde04e26ad53ecd958be9
