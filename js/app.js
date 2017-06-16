8d3260ec70877ed062d014f8f4d1c65b9c0da6a5
Skillz4Killz <admin@ezlgg.com>
2017-06-16 12:57:41 -0400

    completed all project but might have small bug with after new game

--- js/app.js
+++ js/app.js
@@ -1,4 +1,3 @@
-
 (function ticTacToeGame(){
 	const board = document.getElementById('board');
 	const start = document.getElementById('start');
@@ -14,8 +13,7 @@
 	const player1 = document.getElementById('player1');
 	const box = document.getElementsByClassName('boxes')[0].children;
 	const endButton = document.getElementsByClassName('button')[1];
-	
-
+	// on page load prepare start screen and make input for names and checkbox focus on name
 	$(document).ready(() => {
 		finish.style.display = 'none';
 		board.style.display = 'none';
@@ -24,10 +22,9 @@
 		playerX = $('#playerX');
 		$('#player').focus();
 	})
-
-	
-
+//when star clicked choose which program
 	startButton.addEventListener('click', () => {
+//Player vs Computer program initiate
 		if (player.value != '' && document.getElementById('versus').checked) {
 			playerName = player.value;
 			board.style.display = '';
@@ -35,6 +32,7 @@
 			insertName = '<h2>Player Name = ' + playerName + '</h2>';
 			$('#board header h1').after(insertName);
 			player1.className += ' active';
+//player vs player program initiate
 		} else if (player.value != '' && playerX.value != '' && document.getElementById('versus').checked == false) {
 			playerName = player.value;
 			playerXName = playerX.value;
@@ -45,13 +43,14 @@
 			insertName = '<h2>Player X Name = ' + playerXName + '</h2>';
 			$('#board header h1').after(insertName);
 			player1.className += ' active';
+//throw error when no name entered
 		} else {
 			$('#player').focus();
 			player.style.borderColor = 'red';
 			playerX.style.borderColor = 'red';
 		}
 	})
-
+//place background images when hovering over a box depedning on player turn
 	function hovering (x) {
 		x.addEventListener('mouseover', () => {
 			if (player1.className === 'players active') {
@@ -64,40 +63,44 @@
 			})
 		})
 	}
-
+//run program based on which game type 2 player/computer
 	function clicking (x) {
-		x.addEventListener('click', () => {
+//if vs computer after the click assign the value to player O and do Computer turn
+		x.addEventListener('click', function boxFilled() {
 			if (player1.className === 'players active' && document.getElementById('versus').checked) {
 				x.className += ' box-filled-1';
 				player1.className = 'players';
 				player2.className = 'players active';
-				
 				endCounter++;
 				checkEnd();
 				if (endCounter < 9) {
 					vsComputer();
 				}
-				
+			x.removeEventListener('click', boxFilled);
+//if 2 player after click assign the value to player O and make it player 2 turn
 			} else if (player1.className === 'players active') {
 				x.className += ' box-filled-1';
 				player1.className = 'players';
 				player2.className = 'players active';
 				endCounter++;
 				checkEnd();
+			x.removeEventListener('click', boxFilled);
+//if player 2 turn switch to player 1 turn
 			} else {
 				x.className += ' box-filled-2';
 				activateO();
 				endCounter++;
 				checkEnd();
+				x.removeEventListener('click', boxFilled);
 			}
 		})
 	}
-
+//call all functions necessary to make each box have a hover and click function as above
 	for (let i = 0; i < 9; i++) {
 		hovering(box[i]);
 		clicking(box[i]);
 	}
-
+//check if three in a row are filled and end game based on result using 3 parameters provided as 3 boxes to check
 	function checkIfWon(x, y, z) {
         if (box[x].className === "box box-filled-1" && box[y].className === "box box-filled-1" && box[z].className === "box box-filled-1") {
 			oWins();
@@ -109,7 +112,7 @@
             tie();
         }
     }
-    
+//when needing to check for win check each possible winning scenario
     function checkEnd() {
 		checkIfWon(0, 1, 2);
         checkIfWon(3, 4, 5);
@@ -120,20 +123,19 @@
         checkIfWon(0, 4, 8);
         checkIfWon(2, 4, 6);
     }
-
+//switch to player 1 turn
 	function activateO () {
 		player2.className = 'players';
 		player1.className = 'players active';
 	}
-
+//turn off board and give O player victory screen
 	function oWins () {
 		finish.style.display = '';
 		finish.className = 'screen screen-win screen-win-one'
 		board.style.display = 'none';
 		document.getElementsByClassName('message')[0].textContent = "Congrats " + playerName + ' . You are the winner.';
-		
 	}
-
+//turn off board and give X player victory screen or computer victory screen
 	function xWins () {
 		finish.style.display = '';
 		finish.className = 'screen screen-win screen-win-two'
@@ -144,14 +146,14 @@
 		document.getElementsByClassName('message')[0].textContent = "Congrats " + playerXName + ' . You are the winner.';
 		}
 	}
-
+//turn off board and give tie screen
 	function tie () {
 		finish.style.display = '';
 		finish.className = 'screen screen-win screen-win-tie'
 		board.style.display = 'none';
 		document.getElementsByClassName('message')[0].textContent = "It's A Tie!";
 	}
-
+//when New game is clicked clear the board and make it player 1 turn to start again
 	endButton.addEventListener('click', () => {
 		finish.style.display = 'none';
 		board.style.display = '';
@@ -160,8 +162,12 @@
 		}
 		endCounter = 0;
 		activateO();
+		for (let i = 0; i < 9; i++) {
+			hovering(box[i]);
+			clicking(box[i]);
+		}
 	})
-	
+//FOR AI = check if 2 in a row are selected by opponent and block their victory
 	function preventWin (x, y, z) {
 		if (x.className == 'box box-filled-1' && y.className == 'box box-filled-1' && z.className != 'box box-filled-2') {
 			z.className = 'box box-filled-2';
@@ -185,60 +191,54 @@
 			return false;
 		}
 	}
-
+//FOR AI= If there is no move to prevent a victory then make a move in a box
 	function makeMove (x) {
 		if (x.className === 'box') {
 			x.className = 'box box-filled-2';
-			checkEnd();
-			
+			checkEnd();	
 			activateO();
 		}
 	}
-
+/*FOR AI = if computer turn create a var that will call func to check if that three in a row needs to be 
+blocked, if not check next three etc until all possible vitory is made certain is not necessary. Then
+make a while loop to keep making a random number and make a move for it IF that box is empty */
 	function vsComputer () {
 		if (player2.className === 'players active') {
 			console.log('the amount is ' + endCounter);
 			let preventChecker = preventWin(box[0], box[1], box[2]);
 			if (preventChecker === false) {
-				console.log('Didnt blocked top row');
 				preventChecker = preventWin(box[3], box[4], box[5]);
 				if (preventChecker === false) {
-					console.log('Didnt blocked middle row');
 					preventChecker = preventWin(box[6], box[7], box[8]);
 					if (preventChecker === false) {
-						console.log('Didnt blocked bottom row');
 						preventChecker = preventWin(box[0], box[3], box[6]);
 						if (preventChecker === false) {
-							console.log('Didnt blocked left column');
 							preventChecker = preventWin(box[1], box[4], box[7]);
 							if (preventChecker === false) {
-								console.log('Didnt blocked middle column');
 								preventChecker = preventWin(box[2], box[5], box[8]);
+								if (preventChecker === false) {
+									preventChecker = preventWin(box[0], box[4], box[8]);
 									if (preventChecker === false) {
-										console.log('Didnt blocked right column');
-										preventChecker = preventWin(box[0], box[4], box[8]);
-											if (preventChecker === false) {
-												console.log('Didnt blocked 0-8 diagnol');
-												preventChecker = preventWin(box[2], box[4], box[6]);
-											 	if (preventChecker === false) {
-													 console.log('Didnt blocked 2-6 diagnol');
-													 endCounter++;
-													while (endCounter < 9) {
-														let randomNum = Math.floor(Math.random() * 9);
-														console.log(randomNum);
-														if (box[randomNum].className != 'box box-filled-1' && box[randomNum].className != 'box box-filled-2') {
-															console.log('making move ___________________');
-															makeMove(box[randomNum]);
-															break;
-														}
-													}
+										preventChecker = preventWin(box[2], box[4], box[6]);
+										if (preventChecker === false) {
+												endCounter++;
+											while (endCounter < 9) {
+												let randomNum = Math.floor(Math.random() * 9);
+												console.log(randomNum);
+												if (box[randomNum].className != 'box box-filled-1' && box[randomNum].className != 'box box-filled-2') {
+													console.log('making move ___________________');
+													makeMove(box[randomNum]);
+													console.log('the amount is now ' + endCounter);
+													break;
+													
 												}
 											}
+										}
 									}
+								}
 							}
 						}
 					}
-					
 				}
 			}
 		} 
